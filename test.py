b6b4bde6d0e5c135ce4c85f7d89885924a7cc4bf
SP Mohanty <sharada.mohanty@epfl.ch>
2017-11-29 03:08:39 +0100

    Simple working example of seq2seq with same window size

--- test.py
+++ test.py
@@ -1,3 +1,85 @@
-from midi.CrowdAIMidi import CrowdAIMidi
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout, TimeDistributed
+from keras.layers.recurrent import LSTM
 
-midifile = CrowdAIMidi("sample_submission.mid")
+
+# np.set_printoptions(threshold=np.nan)
+
+# import pretty_midi
+
+# m = pretty_midi.PrettyMIDI("sample_submission.mid")
+# pr = m.get_piano_roll()
+#
+# np.save(open("output.npy","wb"), pr)
+
+data = np.load(open("output.npy", "rb")).T
+data = data/128
+
+train_percent = 0.6
+data_train = data[:int(train_percent*data.shape[0])]
+data_test = data[int(train_percent*data.shape[0]):]
+
+MIN_MIDI_NOTE = 24
+MAX_MIDI_NOTE = MIN_MIDI_NOTE + 88
+timesteps = data.shape[0]
+# print data[:,24:24+88].shape
+
+X_WINDOW_SIZE = 1000
+Y_WINDOW_SIZE = 1000
+
+nb_samples = data_train.shape[0] - X_WINDOW_SIZE - Y_WINDOW_SIZE
+
+nb_samples = 2000
+
+x_train_list = [np.expand_dims(
+                np.atleast_2d(
+                    data[i:i+X_WINDOW_SIZE,]
+                ), axis=0) for i in xrange(nb_samples)]
+
+y_train_list = [np.expand_dims(
+                np.atleast_2d(
+                    data[i+X_WINDOW_SIZE:i+X_WINDOW_SIZE+Y_WINDOW_SIZE,]
+                ), axis=0) for i in xrange(nb_samples)]
+
+
+x_train = np.concatenate(x_train_list, axis=0)
+y_train = np.concatenate(y_train_list, axis=0)
+print x_train.shape
+print y_train.shape
+
+# trials = len(x_train_list)
+# features = x_train_list[0].shape[1]
+#
+# print input_mat.shape
+#
+#
+hidden = [64, 64, 64]
+dropouts = [0.5, 0.5, 0.5]
+model = Sequential()
+model.add(LSTM(
+    64,
+    input_shape=(X_WINDOW_SIZE, 128),
+    return_sequences=True
+))
+model.add(Dropout(dropouts[0]))
+
+model.add(LSTM(
+    64,
+    return_sequences=True
+))
+model.add(Dropout(dropouts[1]))
+
+model.add(LSTM(
+    64,
+    return_sequences=True
+))
+model.add(Dropout(dropouts[-1]))
+model.add(TimeDistributed(Dense(128)))
+
+model.compile(loss='mean_squared_error', optimizer='adam')
+print(model.summary())
+
+result = model.fit(x_train, y_train, epochs=20, batch_size=10, verbose=1)
+for value in result[0,:,0]:
+	print('%.1f' % value)
